617257f23198f401f44983fe0248e4246f406512
fterlid <fterlid@gmail.com>
2015-11-29 23:49:00 +0100

    Starting point for implementation

--- spec/sortUtil-spec.js
+++ spec/sortUtil-spec.js
@@ -1,9 +1,14 @@
 'use strict';
 
-import Hello from '../lib/sortUtil';
+import SortUtil from '../lib/sortUtil';
 
 describe('Sort utility tests', function(){
-	it('should do something', function(){
-		//TODO
+	it('sorts an array of strings in ascending, alphabetical order', function(){
+		let sorted = SortUtil.sort(['b', 'a', 'c', 'ab']);
+		
+		expect(sorted[0]).toBe('a');
+		expect(sorted[1]).toBe('ab');
+		expect(sorted[2]).toBe('b');
+		expect(sorted[3]).toBe('c');
 	});
 });
