126875ab0edd2ebb174af5d8442ff7020c683784
fterlid <fterlid@gmail.com>
2015-11-30 00:26:54 +0100

    tab spacing and removed unused variable

--- spec/sortUtil-spec.js
+++ spec/sortUtil-spec.js
@@ -3,29 +3,29 @@
 import SortUtil from '../lib/sortUtil';
 
 describe('Sort utility tests', function(){
-	it('sorts an array of strings in ascending, alphabetical order', function(){
-		let sorted = SortUtil.sort(['b', 'a', 'c', 'ab']);
-		
-		expect(sorted[0]).toBe('a');
-		expect(sorted[1]).toBe('ab');
-		expect(sorted[2]).toBe('b');
-		expect(sorted[3]).toBe('c');
-	});
+    it('sorts an array of strings in ascending, alphabetical order', function(){
+        let sorted = SortUtil.sort(['b', 'a', 'c', 'ab']);
+        
+        expect(sorted[0]).toBe('a');
+        expect(sorted[1]).toBe('ab');
+        expect(sorted[2]).toBe('b');
+        expect(sorted[3]).toBe('c');
+    });
 	
-	it('does not change the order of the original array', function(){
-		let unsorted = ['b', 'a', 'c', 'ab'];
-		let sorted = SortUtil.sort(unsorted);
-		
-		expect(unsorted[0]).toBe('b');
-		expect(unsorted[1]).toBe('a');
-		expect(unsorted[2]).toBe('c');
-		expect(unsorted[3]).toBe('ab');
-	});
+    it('does not change the order of the original array', function(){
+        let unsorted = ['b', 'a', 'c', 'ab'];
+        SortUtil.sort(unsorted);
+        
+        expect(unsorted[0]).toBe('b');
+        expect(unsorted[1]).toBe('a');
+        expect(unsorted[2]).toBe('c');
+        expect(unsorted[3]).toBe('ab');
+    });
 	
-	it('returns a new array', function(){
-		let unsorted = ['b', 'a', 'c', 'ab'];
-		let sorted = SortUtil.sort(unsorted);
-		
-		expect(unsorted).not.toBe(sorted);
-	});
+    it('returns a new array', function(){
+        let unsorted = ['b', 'a', 'c', 'ab'];
+        let sorted = SortUtil.sort(unsorted);
+        
+        expect(unsorted).not.toBe(sorted);
+    });
 });
