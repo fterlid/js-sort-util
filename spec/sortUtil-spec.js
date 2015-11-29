e27c6cd2416bd077e79b8612b2aee0a98f33d121
fterlid <fterlid@gmail.com>
2015-11-30 00:48:14 +0100

    Refactored spec

--- spec/sortUtil-spec.js
+++ spec/sortUtil-spec.js
@@ -2,14 +2,21 @@
 
 import SortUtil from '../lib/sortUtil';
 
-describe('Sort utility tests', function(){
-    it('sorts an array of strings in ascending, alphabetical order', function(){
+describe('Sorting of arrays of strings', function(){
+    it('sorts in ascending, alphabetical order', function(){
         let sorted = SortUtil.sort(['b', 'a', 'c', 'ab']);
         
         expect(sorted[0]).toBe('a');
         expect(sorted[1]).toBe('ab');
         expect(sorted[2]).toBe('b');
         expect(sorted[3]).toBe('c');
+    });
+    
+    it('returns a new array', function(){
+        let unsorted = ['b', 'a', 'c', 'ab'];
+        let sorted = SortUtil.sort(unsorted);
+        
+        expect(unsorted).not.toBe(sorted);
     });
 	
     it('does not change the order of the original array', function(){
@@ -20,12 +27,5 @@ describe('Sort utility tests', function(){
         expect(unsorted[1]).toBe('a');
         expect(unsorted[2]).toBe('c');
         expect(unsorted[3]).toBe('ab');
-    });
-	
-    it('returns a new array', function(){
-        let unsorted = ['b', 'a', 'c', 'ab'];
-        let sorted = SortUtil.sort(unsorted);
-        
-        expect(unsorted).not.toBe(sorted);
     });
 });
