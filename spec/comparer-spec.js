edb181ed5e130317804a6a59b320ee4ff55bb8a4
fterlid <fterlid@gmail.com>
2015-12-09 22:22:46 +0100

    removed unused compare method

--- spec/comparer-spec.js
+++ spec/comparer-spec.js
@@ -1,21 +1,6 @@
 'use strict';
 import Comparer from '../lib/comparer';
 
-describe('compare function', () => {
-    
-    it('compares letter strings in ascending, alphabetical order', () => {
-        expect(Comparer.compare('a', 'b')).toBeLessThan(0);
-        expect(Comparer.compare('b', 'a')).toBeGreaterThan(0);
-        expect(Comparer.compare('z', 'z')).toBe(0);
-    });
-    
-    it('compares numbers in ascending, numerical order', () => {
-        expect(Comparer.compare(1, 3)).toBeLessThan(0);
-        expect(Comparer.compare(5, 4)).toBeGreaterThan(0);
-        expect(Comparer.compare(42, 42)).toBe(0);
-    });
-});
-
 describe('stableCompare function', () => {
     
     it('compares letter strings in ascending, alphabetical order', () => {
