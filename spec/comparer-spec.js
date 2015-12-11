607865f8f0dcf492cc2660170912e04aac432c75
fterlid <fterlid@gmail.com>
2015-12-11 22:40:55 +0100

    compare numeric strings numerically

--- spec/comparer-spec.js
+++ spec/comparer-spec.js
@@ -14,6 +14,23 @@ describe('compare function', () => {
         expect(Comparer.compare({key: 'a', index: 2}, {key: 'a', index: 2})).toBe(0);
     });
     
+    it('compares number strings in ascending, numerical order', () => {
+        expect(Comparer.compare({ key: '2', index: 2 }, { key: '10', index: 1 })).toBeLessThan(0);
+        expect(Comparer.compare({ key: '40', index: 2 }, { key: '3', index: 3 })).toBeGreaterThan(0);
+        expect(Comparer.compare({ key: '5', index: 8 }, { key: '5', index: 8 })).toBe(0);
+    });
+    
+    it('compares number strings and numbers in ascending, numerical order', () => {
+        expect(Comparer.compare({ key: '2', index: 2 }, { key: 10, index: 1 })).toBeLessThan(0);
+        expect(Comparer.compare({ key: 2, index: 2 }, { key: '10', index: 1 })).toBeLessThan(0);
+        
+        expect(Comparer.compare({ key: '40', index: 2 }, { key: 3, index: 3 })).toBeGreaterThan(0);
+        expect(Comparer.compare({ key: 40, index: 2 }, { key: '3', index: 3 })).toBeGreaterThan(0);
+        
+        expect(Comparer.compare({ key: '5', index: 8 }, { key: 5, index: 8 })).toBe(0);
+        expect(Comparer.compare({ key: 5, index: 8 }, { key: '5', index: 8 })).toBe(0);
+    });
+    
     it('compares numbers in ascending, numerical order', () => {
         expect(Comparer.compare({key: 1, index: 2}, {key: 3, index: 1})).toBeLessThan(0);
         expect(Comparer.compare({key: 5, index: 2}, {key: 4, index: 3})).toBeGreaterThan(0);
