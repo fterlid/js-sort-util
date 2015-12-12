e562e32fc4d93871ff00bad4951a68d601d6a502
fterlid <fterlid@gmail.com>
2015-12-12 01:38:20 +0100

    Added ascending arg to facade signature

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -8,10 +8,17 @@ describe('Sort function', () => {
         spyOn(Sorter, 'sort').and.returnValue(expected);
         
         let actual1 = Facade.sort([]);
+        expect(Sorter.sort).toHaveBeenCalledWith([], undefined, true);
+        
         let actual2 = Facade.sort([], '');
+        expect(Sorter.sort).toHaveBeenCalledWith([], '', true);
+        
+        let actual3 = Facade.sort([], '', false);
+        expect(Sorter.sort).toHaveBeenCalledWith([], '', false);
         
         expect(actual1).toBe(expected);
         expect(actual2).toBe(expected);
+        expect(actual3).toBe(expected);
     });
     
     it('throws if first argument is not an Array', () => {
@@ -42,6 +49,23 @@ describe('Sort function', () => {
         expect(callSort).toThrowError(expectedErrorText);
         
         arg = 42;
+        expect(callSort).toThrowError(expectedErrorText);
+    });
+    
+    it('throws if ascending argument is provided and is not a boolean', () => {
+        let expectedErrorText = 'sort: Expected a boolean as third argument.';
+        let arg = [];
+        let callSort = () => Facade.sort([], null, arg);
+        
+        expect(callSort).toThrowError(expectedErrorText);
+        
+        arg = {};
+        expect(callSort).toThrowError(expectedErrorText);
+        
+        arg = 42;
+        expect(callSort).toThrowError(expectedErrorText);
+        
+        arg = 'a string';
         expect(callSort).toThrowError(expectedErrorText);
     });
 });
